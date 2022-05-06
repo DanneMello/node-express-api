82e4e99e63fbc769be16acbd398e8298618da88c
Daniel Fures Melo <daniel.melo@jointecnologia.com.br>
2022-05-06 18:12:16 -0300

    CAP-2
    Criado CRUD com os métodos get, post, put e delete.

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js';
 
-const rotas = {
-    '/': 'Curso de NodeJS',
-    '/livros': 'Esses são os livros disponíveis no momento.',
-    '/autores': 'Essa é a galera que faz a \'mágica\' acontecer.',
-    '/forum': 'Aqui a galera se ajuda',
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text-plain'});
-    res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
-    console.log(`Servidor escutanto em http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
 });
