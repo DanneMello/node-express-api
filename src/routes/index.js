072e03e7819f3c7ca65eff2b606823c18876852f
Daniel Fures Melo <daniel.melo@jointecnologia.com.br>
2022-05-08 12:16:38 -0300

    CAP-5
    Fazendo relacionamentos entre entidades.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // Imports necessários 
 import express from "express";
 import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
@@ -10,7 +11,8 @@ const routes = (app) => {
     // Aponta as rotas a serem utilizadas
     app.use(
         express.json(),
-        livros
+        livros,
+        autores
     );
 }
 
